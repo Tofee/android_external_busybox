0434c5e372737d0f08ed3889c0e5d8addc3cfa3d
Christophe Chapuis <chris.chapuis@gmail.com>
2016-11-04 18:11:42 +0000

    Fix __err_no compilation error for mako
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- android/libc/__set_errno.c
+++ android/libc/__set_errno.c
@@ -28,8 +28,6 @@
 
 #include <errno.h>
 
-#ifdef __LP64__
-
 /* __set_errno was mistakenly exposed in <errno.h> in the 32-bit NDK.
  * but is no more present for 64-bit targets!
  */
@@ -39,5 +37,3 @@ long __set_errno(int n)
     errno = n;
     return -1;
 }
-
-#endif
